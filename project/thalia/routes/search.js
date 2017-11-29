a4c27fb138f79fb2b5799347b71d5c2f8ed66c3f
Tahj Atkinson <tahj.atkinson@gmail.com>
2017-11-29 15:13:23 -0600

    final commit

--- project/thalia/routes/search.js
+++ project/thalia/routes/search.js
@@ -6,7 +6,27 @@ let theatre = main.theatre;
 let theatreFactory = main.theatreFactory;
 
 router.get('/', function(req, res, next) {
-//Search by topic or key word expect params
+    try {
+        let topic = req.query.topic;
+        let key = req.query.key;
+        let order = {};
+        switch (topic) {
+            case "order":
+                order = theatre.getShows().find(s => {
+                    return s.getOrders().find(o => o.id == key);
+
+                });
+                break;
+        }
+        let output = [];
+        output.push(order);
+
+
+        res.send({ "orders": output }).status(200);
+    } catch (e) {
+        res.send().status(500);
+    }
+
 });
 
 module.exports = router;
